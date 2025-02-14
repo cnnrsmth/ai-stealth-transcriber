73efe23ab6f04c59c114857878e2061efa3e07e9
cs862 <59978170+cs862@users.noreply.github.com>
2025-02-14 00:50:16 +0000

    build fixes

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-set -eux
+set -eux  # Exit on error, print each command
 
-# Install PortAudio (required for PyAudio and Sounddevice)
+# Install system dependencies, including PortAudio
 apt-get update && apt-get install -y portaudio19-dev
+
+# Install Python dependencies
+pip install --upgrade pip
+pip install -r requirements.txt
